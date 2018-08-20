9e2561516c4a1dc3f941f4cc59dd661a8b4007cd
liuying <liuyingover@163.com>
2018-08-20 10:48:55 +0800

    add admin support

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from article.models import Image, Article
+
+
+@admin.register(Image)
+class ImgAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'type', 'create_time')
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'cate', 'states', 'is_about', 'create_time', 'update_time')
